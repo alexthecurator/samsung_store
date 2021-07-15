a52a48f04b8c779b6c6c9603ee5734e055922710
alexthecurator <edge.works.curator@gmail.com>
2021-07-15 17:08:41 +0300

    Fixing View issue..

--- samsung_store/store/models.py
+++ samsung_store/store/models.py
@@ -4,34 +4,31 @@ from django.utils import timezone
 
 # Create your models here.
 class Phone(models.Model):
-    # from_store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    # store = models.ForeignKey('store.Store', on_delete=models.CASCADE, blank=unique_id())
     name = models.CharField(max_length=100)
     about = models.TextField(max_length=250)
     image = models.FileField(upload_to='assets/phones', max_length=100)
     rating = models.IntegerField(blank=True)
     price = models.FloatField(max_length=255, blank=True)
-    store = models.ForeignKey('store.Store', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"Name: {self.name}, Ratings: {self.rating} stars, Price: {self.price}$"
+        return f"Name: {self.name},  Ratings: {self.rating} stars,  Price: {self.price}$"
         
-
 class Store(models.Model):
-    sale_id = models.CharField(max_length=150)
+    thestore_id = models.CharField(max_length=150, blank=True)
     name = models.CharField(max_length=150)
-    date = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f"Store: {self.name}, Date: {self.date}"
+    date = models.DateTimeField(auto_now=True)
     
     def save(self, *args, **kwargs):
-        if self.sales_id == "":
-            self.sales_id = unique_id()
+        if self.thestore_id == "":
+            self.thestore_id = unique_id()
         if self.date == "":
-            self.date = timezone.now() ## Adds the current timestap
+            self.date = timezone.now()
         return super().save(*args, **kwargs)
-        
+    
+    def __str__(self):
+        return f"Store: {self.name},  Date: {self.date}"    
 class Buyers(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
--- samsung_store/store/views.py
+++ samsung_store/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
+from .models import Phone, Store
 # Create your views here.
+
+def home_view(request):
+    title = "Home"
+    context = {
+        'title':title,
+    }
+    return render(request, 'home.html', context)
+
+def product_view(request):
+    qs = Phone.objects.all()
+    return render(request, 'home.html', {'qs':qs})
