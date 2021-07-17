48ba1fc6092d688198a3c3bcb3a807c75a2e147c
alexthecurator <edge.works.curator@gmail.com>
2021-07-18 00:04:54 +0300

    Setting up templates.. ✈

--- samsung_store/store/urls.py
+++ samsung_store/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, showcase_view
 
 app_name='store'
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('showcase', showcase_view, name="showcase")
 ]
--- samsung_store/store/views.py
+++ samsung_store/store/views.py
@@ -9,3 +9,10 @@ def home_view(request):
         'title':title,
     }
     return render(request, 'store/home.html', context)
+
+def showcase_view(request):
+    title = "Showcase"
+    context = {
+        'title':title,
+    }
+    return render(request, 'store/showcase.html', context)
